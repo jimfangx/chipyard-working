82fddeda3647a922e10ec7b7fb11256ecc90fe4c
Jim Fang <yf328@eecs.berkeley.edu>
2026-07-06 16:40:28 -0700

    further firesim test fixes

--- generators/firechip/chip/src/main/makefrag/firesim/build.mk
+++ generators/firechip/chip/src/main/makefrag/firesim/build.mk
@@ -69,4 +69,17 @@ SHUTTLE_VECTOR_VERILATOR_FLAGS = \
 
 # AJG: this must be evaluated after verilog generation to work (hence the =)
 EXTRA_VERILATOR_FLAGS = \
-	$(shell if grep -iq "module.*cva6" $(simulator_verilog); then echo "$(CVA6_VERILATOR_FLAGS)"; elif grep -iq "module.*gemmini" $(simulator_verilog); then echo "$(GEMMINI_VERILATOR_FLAGS)"; elif grep -iq "module.*shuttle.*vector" $(simulator_verilog); then echo "$(SHUTTLE_VECTOR_VERILATOR_FLAGS)"; else echo "$(DEFAULT_MIDAS_VERILATOR_FLAGS)"; fi)
+	$(shell \
+		if grep -iq "module.*cva6" $(simulator_verilog); then \
+			echo "$(CVA6_VERILATOR_FLAGS)"; \
+		elif echo "$(TARGET_CONFIG)" | grep -Eiq "Gemmini"; then \
+			echo "$(GEMMINI_VERILATOR_FLAGS)"; \
+		elif grep -iq "module.*gemmini" $(simulator_verilog); then \
+			echo "$(GEMMINI_VERILATOR_FLAGS)"; \
+		elif echo "$(TARGET_CONFIG)" | grep -Eiq "Shuttle"; then \
+			echo "$(SHUTTLE_VECTOR_VERILATOR_FLAGS)"; \
+		elif grep -Eiq "module.*shuttle.*vector|shuttle_tile.*vector_unit|vector_unit\.vu" $(simulator_verilog); then \
+			echo "$(SHUTTLE_VECTOR_VERILATOR_FLAGS)"; \
+		else \
+			echo "$(DEFAULT_MIDAS_VERILATOR_FLAGS)"; \
+		fi)
